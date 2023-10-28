46c38ac4605a02ff8c36dc68356c6f651f6f4409
TL93OV <jixin1207@gmail.com>
2023-10-28 15:51:18 +0200

    poly korrekt

--- X1/polxgon.py
+++ X1/polxgon.py
@@ -10,4 +10,9 @@ class Triangle:
 
 my_polygon = Polygon(sides=4, color='kék')
 
-print(f"ennek a polygonnak {my_polygon.sides} oldala van és {my_polygon.color} színű")
+print(f"ennek a polygonnak {my_polygon.sides} oldala van és {my_polygon.color} színű és oldalszélessége {my_polygon.oldalszelszelesseg}")
+
+my_triangle = Polygon(3)
+
+
+print(f"ennek a polygonnak {my_triangle.sides} oldala van és {my_triangle.color} színű és oldalszélessége {my_triangle.oldalszelszelesseg}")
